983a5129510f643fc349af9d48587b055c77f52d
Dev Teertha <devteertha28@gmail.com>
2021-07-09 00:17:34 +0600

    design change

--- src/components/TagInput.js
+++ src/components/TagInput.js
@@ -47,16 +47,16 @@ const TagInput = ({ get, postToDatabaseHandler }) => {
     }
     return (
         <div className="container">
-            <div className="tag-header">
-                <h4>TAGS</h4>
-                <p>Select time for your event</p>
+            <div className="tag-header mt-12">
+                <h4 className="text-green-600 text-2xl font-bold mb-1 text-gray-700">TAGS</h4>
+                <p className="mb-2 text-gray-600">Select time for your event</p>
             </div>
-            <div id="tagInput" className="tagInput">
+            <div id="tagInput" className="tagInput p-5 rounded-lg transition duration-500 ease-in-out bg-gray-100 border-b-2 focus:border-green-600 hover:border-green-600">
                 <ul>
                     {
                         tags.map((tag, index) => {
                             return (
-                                <li key={index}>
+                                <li className="mx-1 my-2 hover:bg-green-700 bg-green-600 text-white px-3 py-1 rounded-lg" key={index}>
                                     <span>{tag}</span>
                                     <button onClick={() => removeTags(index)}>
                                         <FontAwesomeIcon icon={faTimes} />
@@ -66,14 +66,14 @@ const TagInput = ({ get, postToDatabaseHandler }) => {
                         })
                     }
                 </ul>
-                <input value={value} onKeyPress={keyboardHandler} onChange={(e) => changeHandler(e)} id="tag-input" type="text" placeholder="ADD TAGS" />
+                <input className="mt-2 text-lg text-gray-800" value={value} onKeyPress={keyboardHandler} onChange={(e) => changeHandler(e)} id="tag-input" type="text" placeholder="ADD TAGS" />
             </div>
             {
                 isSuggest && <div className="tagSuggestion">
                     <div className="suggest-lists">
                         {
-                            suggestion.length > 0 ? suggestion.map(s => <p onClick={() => addTagFromSuggest(s)} className="suggest-list">{s}</p>) :
-                                <p className="notFound">Not Found</p>
+                            suggestion.length > 0 ? suggestion.map(s => <p onClick={() => addTagFromSuggest(s)} className="py-3 px-3 hover:bg-green-600 hover:text-white rounded text-lg my-2 cursor-pointer">{s}</p>) :
+                                <p className="text-gray-600 text-2xl ml-5 mt-5">Not Found</p>
                         }
                     </div>
                 </div>
